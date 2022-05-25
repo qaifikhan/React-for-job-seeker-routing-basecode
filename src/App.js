c2e34069eec4c5dd5351babeab67c22a5a70b481
Qaifi <qaifikhan@gmail.com>
2022-05-25 11:19:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import Topbar from './Topbar/Topbar';
+import HomePage from './HomePage/HomePage';
+import BlogPage from './BlogPage/BlogPage';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar />
+
+      <main className={classes.MainContainer}>
+        <HomePage />
+        <BlogPage />
+        <LoginPage />
+      </main>
     </div>
   );
 }
